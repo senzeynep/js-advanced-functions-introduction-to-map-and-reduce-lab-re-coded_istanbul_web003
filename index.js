130dc6bcdad3e174c5db666b34e866e520732a1b
zeynepsen <zszynpsen@gmail.com>
2020-10-23 00:29:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// Your code here
+// map-like functions
+
+function mapToNegativize(sourceArray) {
+    let targetArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        targetArray.push(sourceArray[i] * -1)
+    }
+    return targetArray
+}
+
+function mapToNoChange(sourceArray) {
+    let targetArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        targetArray.push(sourceArray[i])
+    }
+    return targetArray
+}
+
+function mapToDouble(sourceArray) {
+    let targetArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        targetArray.push(sourceArray[i] * 2)
+    }
+    return targetArray
+}
+
+
+function mapToSquare(sourceArray) {
+    let targetArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        targetArray.push(sourceArray[i] ** 2)
+    }
+    return targetArray
+}
+
+// reduce-like functions
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let total = 1
+    if (startingPoint!= undefined)
+        total += startingPoint
+        for (let i = 1; i < sourceArray.length; i++) {
+            total += sourceArray[i]
+            }
+        return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let value = false
+    for (let i = 0; i < sourceArray.length; i++) {
+        if(sourceArray[i])
+        value = true
+        else {
+            value = false
+        }
+    }
+    return value
+}
+
+
+function reduceToAnyTrue(sourceArray) {
+    let value = false
+    for (let i = 0; i < sourceArray.length; i++) {
+        if(sourceArray[i]) 
+        return true
+        } 
+            return false
+}
